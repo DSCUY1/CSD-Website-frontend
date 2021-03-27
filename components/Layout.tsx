70863bfab0221a8f62b63399b0aaa016dfcb5083
dilane3 <komboudilane125@gmail.com>
2021-03-27 11:52:31 +0100

    restructuration du layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,8 +2,9 @@ import React, {Component} from 'react';
 import Image from 'next/image';
 import Head from 'next/head';
 import ControlledCarousel from './toolsComponent/customCarousel';
-import NavbarCollapse from './toolsComponent/customCollapse';
-import CustomCardFooter from './toolsComponent/customCardFooter';
+import Footer from './footer';
+import NavbarNavigation from './navbar';
+import NavbarNavigationMobile from './navbarMobile';
 
 class Layout extends Component {
   constructor() {
@@ -19,19 +20,6 @@ class Layout extends Component {
   }
 
   render() {
-    let header_card1 = <>
-      <img src="/static/logo_fac.png" alt="..." style={{ height: "60px", width: "60px" }}/>
-      <div className="ps-2 d-flex header-body-card ml-3 text-light">
-        <h5 className="fs-18">Departement d'informatique</h5>
-        <h6 className="fs-15 op-6 text-light">Université de Yaoundé I</h6>
-      </div>
-    </>;
-    let header_card2 = <h2 className="fs-18 op-6 text-light">INFORMATIONS</h2>;
-    let header_card3 = <h2 className="fs-18 op-6 text-light">NOS SITES WEB</h2>;
-
-    let footer_card1 = <a href="" className="btn-card btn btn-link p-2 text-decoration-none text-white">Mon portail</a>;
-    let footer_card2 = <a href=""><img className="m-0" src="/static/logo_playStore.png" style={{ height: "30px", width: "100px" }} alt="" /></a>;
-
     return (
       <>
         <Head>
@@ -44,65 +32,8 @@ class Layout extends Component {
           <div className="row">
             <div className={`menu-navbar ${this.state.toggleMenuMobile}`}>
               <span className="close-mobile-menu" onClick={() => this.handleToggleMenuMobile("")}>&times;</span>
-              <div className="list-group navbar-nav-mobile">
-                <NavbarCollapse
-                  trigger={<a className="list-group-item list-group-item-action active" href="#">Acceuil</a>}
-                >
-                  <div className="list-group">
-                    <a className="list-group-item list-group-item-action active-sublist" href="# ">Barbillard</a>
-                    <a className="list-group-item list-group-item-action " href="# ">Evenements</a>
-                  </div>
-                </NavbarCollapse>
 
-                <NavbarCollapse
-                  trigger={<a className="list-group-item list-group-item-action" href="#">Departement</a>}
-                >
-                  <div className="list-group">
-                    <a className="list-group-item list-group-item-action" href="# ">Présentation</a>
-                    <a className="list-group-item list-group-item-action " href="# ">Condition d'admission</a>
-                    <a className="list-group-item list-group-item-action active-sublist" href="# ">Coodonées</a>
-                    <a className="list-group-item list-group-item-action" href="# ">Nos poles</a>
-                    <a className="list-group-item list-group-item-action" href="# ">Nos enseignants</a>
-                    <a className="list-group-item list-group-item-action" href="# ">Nos options et débouché</a>
-                  </div>
-                </NavbarCollapse>
-
-                <NavbarCollapse
-                  trigger={<a className="list-group-item list-group-item-action" href="#">Informations</a>}
-                >
-                  <p>Hello</p>
-                </NavbarCollapse>
-
-                <NavbarCollapse
-                  trigger={<a className="list-group-item list-group-item-action" href="#">Formation</a>}
-                >
-                  <p>Hello</p>
-                </NavbarCollapse>
-
-                <NavbarCollapse
-                  trigger={<a className="list-group-item list-group-item-action" href="#">Espace Etudiants</a>}
-                >
-                  <p>Hello</p>
-                </NavbarCollapse>
-
-                <NavbarCollapse
-                  trigger={<a className="list-group-item list-group-item-action" href="#">Ancien Etudiants</a>}
-                >
-                  <p>Hello</p>
-                </NavbarCollapse>
-
-                <NavbarCollapse
-                  trigger={<a className="list-group-item list-group-item-action" href="#">Nos Partenaires</a>}
-                >
-                  <p>Hello</p>
-                </NavbarCollapse>
-
-                <NavbarCollapse
-                  trigger={<a className="list-group-item list-group-item-action" href="#">Blog</a>}
-                >
-                  <p>Hello</p>
-                </NavbarCollapse>
-              </div>
+              <NavbarNavigationMobile />
             </div>
             <div className="col-sm">
               <div className="container-fluid d-flex justify-content-between header-section">
@@ -116,45 +47,7 @@ class Layout extends Component {
                 </div>
               </div>
 
-              <div id="navbar">
-                <nav className="navbar-expand-lg navbar-light d-flex first-navbar">
-                  <button className="navbar-toggler" type="button" onClick={() => this.handleToggleMenuMobile("toggle-menu-mobile")}>
-                    <span className="navbar-toggler-icon"></span>
-                  </button>
-
-                  <div className="navbar-nav" id="parent_menu">
-                    <a className="nav-link active" aria-current="page" href="#">Acceuil</a>
-                    <a className="nav-link" aria-current="page" href="#">Département</a>
-                    <a className="nav-link" aria-current="page" href="#">Informations</a>
-                    <a className="nav-link" aria-current="page" href="#">Formation</a>
-                    <a className="nav-link" aria-current="page" href="#">Espace Etudiants</a>
-                    <a className="nav-link" aria-current="page" href="#">Ancien Etudiants</a>
-                    <a className="nav-link" aria-current="page" href="#">Nos Partenaires</a>
-                    <a className="nav-link" aria-current="page" href="#">Blog</a>
-                  </div>
-
-                  <form className="d-flex form-search">
-                    <input className="form-control search_bar" type="search" placeholder="Search" aria-label="Search" />
-                  </form>
-                </nav>
-                <hr />
-                <nav className="navbar-expand-lg navbar-light d-flex last-navbar">
-                  <div className="collapse navbar-collapse sub_menu d-flex flex-column align-items-start" id="navbarNavAltMarkup ">
-                    <div className="navbar-nav d-none" id="home ">
-                      <a className="nav-link active" aria-current="page " href="# ">Barbillard</a>
-                      <a className="nav-link " aria-current="page " href="# ">Evenements</a>
-                    </div>
-                    <div className="navbar-nav" id="departement ">
-                      <a className="nav-link" aria-current="page " href="# ">Présentation</a>
-                      <a className="nav-link " aria-current="page " href="# ">Condition d'admission</a>
-                      <a className="nav-link active" aria-current="page " href="# ">Coodonées</a>
-                      <a className="nav-link " aria-current="page " href="# ">Nos poles</a>
-                      <a className="nav-link " aria-current="page " href="# ">Nos enseignants</a>
-                      <a className="nav-link " aria-current="page " href="# ">Nos options et débouché</a>
-                    </div>
-                  </div>
-                </nav>
-              </div>
+              <NavbarNavigation ToggleMenuMobile={this.handleToggleMenuMobile} />
 
               <ControlledCarousel />
 
@@ -162,58 +55,7 @@ class Layout extends Component {
                 {this.props.children}
               </div>
 
-
-
-              <div className="container-fluid">
-                <div className="row">
-                  <div className="col-sm footer-links">
-                    <div className="row">
-                      <CustomCardFooter
-                        headerCard={header_card1}
-                        footerCard={footer_card1}
-                        className="col-lg-4 col-md-6 col-sm-12"
-                      >
-                        <a href="#" className="text-decoration-none text-white">Présentation</a><br/>
-                        <a href="#" className="text-decoration-none text-white">Conditions d'admission</a><br/>
-                        <a href="#" className="text-decoration-none text-white">Nos pôles</a><br/>
-                        <a href="#" className="text-decoration-none text-white">Nos enseignants</a><br/>
-                        <a href="#" className="text-decoration-none text-white">Options & débouchés</a><br/>
-                      </CustomCardFooter>
-
-                      <CustomCardFooter
-                        headerCard={header_card2}
-                        footerCard={footer_card2}
-                        className="col-lg-4 col-md-6 col-sm-12"
-                      >
-                        <a href="#" className="text-decoration-none text-white">Babillard</a><br/>
-                        <a href="#" className="text-decoration-none text-white">Espaces Etudiants</a><br/>
-                        <a href="#" className="text-decoration-none text-white">Anciens Etudiants</a><br/>
-                        <a href="#" className="text-decoration-none text-white">Nos partenaires</a><br/>
-                        <a href="#" className="text-decoration-none text-white">Blog</a><br/>
-                      </CustomCardFooter>
-
-                      <CustomCardFooter
-                        headerCard={header_card3}
-                        className="col-lg-4 col-md-6 col-sm-12"
-                      >
-                        <a href="#" className="text-decoration-none text-white">Faculté des Sciences</a><br/>
-                          <a href="#" className="text-decoration-none text-white">Ecole Doctorale CRFD Sciences et Technologies</a><br/>
-                          <a href="#" className="text-decoration-none text-white">Université de Yaoundé I</a><br/>
-                          <a href="#" className="text-decoration-none text-white">MINESUP</a><br/>
-                      </CustomCardFooter>
-                    </div>
-                  </div>
-                </div>
-                <div className="row">
-                  <div className="col-sm footer-info">
-                    <p className="d-flex text-light fs-12">Tous droits réservés © Université de Yaoundé I</p>
-                    <div className="d-flex">
-                        <p className="text-light fs-12">Designed & Developed by</p>
-                        <img src="/static/logo_dsc.png" style={{ height: "40px" }} alt="" />
-                    </div>
-                  </div>
-                </div>
-              </div>
+              <Footer />
             </div>
           </div>
         </div>
@@ -223,11 +65,3 @@ class Layout extends Component {
 }
 
 export default Layout;
-
-// <div class="row fs-13 text-white ff-rr back-black pe-3 ps-3">
-//             <p class="col-md-7 d-flex pt-md-3 ps-md-5">Tous droits réservés © Université de Yaoundé I</p>
-//             <div class="col-md-5 d-md-flex flex-md-row-reverse">
-//                 <img src="../image/logo_dsc.png" alt="">
-//                 <p class="pt-3 me-2">Designed & Developed by</p>
-//             </div>
-//         </div>
