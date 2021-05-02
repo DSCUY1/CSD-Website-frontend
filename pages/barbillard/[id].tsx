f334d57269faa673d582e46cc9ce026d1b2e43e5
dilane3 <komboudilane125@gmail.com>
2021-05-02 20:35:28 +0100

    update

---
+++ pages/barbillard/[id].tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../../components/Layout';
+
+function BarbillardPage() {
+  return (
+    <Layout title="barbillard" nav={{ mainMenu: "accueil", subMenu: "barbillard" }}>
+      <div>un element barbillard en particulier</div>
+    </Layout>
+  );
+}
+
+export default BarbillardPage;
